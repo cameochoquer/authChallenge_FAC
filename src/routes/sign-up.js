f0365d0c06f84a7317b1011556d3cbf0061359c7
cameochoquer <cjchoquer@gmail.com>
2023-05-26 13:26:59 +0100

    challenge 2 complete, not tested

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,4 +1,8 @@
+const { createSession } = require("../model/session.js");
+const { createUser } = require("../model/user.js");
 const { Layout } = require("../templates.js");
+const bcrypt = require("bcryptjs");
+
 
 function get(req, res) {
   const title = "Create an account";
@@ -21,21 +25,34 @@ function get(req, res) {
   const body = Layout({ title, content });
   res.send(body);
 }
-
-function post(req, res) {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).send("Bad input");
-  } else {
-    res.send("to-do");
     /**
      * [1] Hash the password
+  
      * [2] Create the user in the DB
+
      * [3] Create the session with the new user's ID
+
      * [4] Set a cookie with the session ID
      * [5] Redirect to the user's confession page (e.g. /confessions/3)
      */
-  }
-}
+const post = (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send("Bad input")
+    return;
+  }   bcrypt.hash(password, 12)
+  .then((hash) => createUser(email, hash))
+  .then((user) => {
+    const sessionId = createSession(user.id);
+    res.set(`set-cookie`, `${sessionId}; HttpOnly; Max-Age=60; SameSite=Lax`);
+    res.redirect(`/confessions/${user.id}`);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  });
+};
+
+  
 
 module.exports = { get, post };
